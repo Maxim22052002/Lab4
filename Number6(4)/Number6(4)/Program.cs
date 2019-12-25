3c65474ddd4990d7aa582a770ae3d706786341eb
Maxim22052002 <www.maksim.azot.dn@gmail.com>
2019-12-25 21:19:20 +0300

    T6

---
+++ Number6(4)/Number6(4)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace Number6_4_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string newstr = string.Empty;
+            
+            
+            Regex RegExp = new Regex(@"\-?\d+\s*[+|\-|*|/]\s*\-?\d+\s*[=]\s*\-?\d+");
+            foreach (Match match in RegExp.Matches(str))
+            {
+                newstr += match;
+                Console.WriteLine(match);
+
+
+            }
+                
+                str = newstr.Replace(" ", string.Empty);
+              //  Console.WriteLine(str);
+                string[] str2 = str.Split('+',  '=', '*', '/');
+            
+                int[] vals = new int[str2.Length];
+                for (int i = 0; i < str2.Length; i++)
+                {
+                    vals[i] = Convert.ToInt32(str2[i]);
+                    Console.WriteLine(vals[i] + "  ");
+                }
+            
+               
+            
+            
+        } 
+    }
+}
