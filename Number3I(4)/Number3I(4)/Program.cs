ab30ad700ec733cd13ed068d7be958ccdd744504
Maxim22052002 <www.maksim.azot.dn@gmail.com>
2019-12-25 21:17:39 +0300

    T3I

---
+++ Number3I(4)/Number3I(4)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Number3I_4_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Regex RegExp = new Regex(@"\b[0-23]{1,2}\:[0-59]{1,2}\:[0-59]{1,2}\b");
+            // Regex RegExp = new Regex(@"\b\d{3}\.\d{3}\.\d\.\d{2}\b");
+          //  Regex RegExp = new Regex(@"\w+:\s+[int|short|byte]+\s+\[\d+\]");
+            foreach (Match match in RegExp.Matches(str))
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
