71d67fe54f4c7d2e30bc6b367956aeab6da89318
Maxim22052002 <www.maksim.azot.dn@gmail.com>
2019-12-25 21:18:46 +0300

    T5

---
+++ Number5(4)/Number5(4)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Number5_4_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            ResultReg(str);
+            ResultMethods(str);
+        }
+        static void ResultReg(string str)
+        {
+            Regex RegExp = new Regex(@"\b[A-Z][a-z]*\d{2}\b");
+            foreach (Match match in RegExp.Matches(str))
+            {
+                Console.WriteLine(match);
+            }
+        }
+        static void ResultMethods(string str)
+        {
+            string[] arr = str.Split(new char[] { ' ', ',', '-', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                char[] array = arr[i].ToCharArray();
+                if (Char.IsUpper(array[0]) &&Char.IsLetter(array[array.Length-3])&& Char.IsDigit(array[array.Length - 1]) && Char.IsDigit(array[array.Length - 2]))
+                {
+                    string newstr = new string(array);
+                    Console.WriteLine(newstr);
+                }
+            }
+        }
+    }
+}
